6afe304d089fbaa01269acc7dc95b12c2ffae085
astaria <astaria@gmx.de>
2017-06-18 11:25:22 +0200

    debugger breakpoint enhancements

--- Jint/Runtime/Debugger/BreakPoint.cs
+++ Jint/Runtime/Debugger/BreakPoint.cs
@@ -5,15 +5,17 @@
         public int Line { get; set; }
         public int Char { get; set; }
         public string Condition { get; set; }
+        public string Source { get; set; }
 
-        public BreakPoint(int line, int character)
+        public BreakPoint(int line, int character, string source)
         {
             Line = line;
             Char = character;
+            Source = source;
         }
 
-        public BreakPoint(int line, int character, string condition)
-            : this(line, character)
+        public BreakPoint(int line, int character, string source, string condition)
+            : this(line, character, source)
         {
             Condition = condition;
         }
